88f1ed982e4d3839111fb9ff98c97139ac96c3be
Sri Preetham Sai Ravula <preethamsai.ravula@gmail.com>
2025-06-29 04:31:36 -0400

    changes to incorporate text only, image only model modes

--- src/multimodal_model.py
+++ src/multimodal_model.py
@@ -5,8 +5,17 @@ from transformers import AutoModel # Pretrained text encoders
 
 
 class MediLLMModel(nn.Module):
-    def __init__(self, text_model_name="emilyalsentzer/Bio_ClinicalBERT", num_classes=3, dropout=0.3, hidden_dim=256): # Bio_ClinicalBERT is a pretrained model on clinical notes, output to 3 classes i.e triage levels
+    def __init__(
+        self, 
+        text_model_name="emilyalsentzer/Bio_ClinicalBERT", # Bio_ClinicalBERT is a pretrained model on clinical notes, output to 3 classes i.e triage levels
+        num_classes=3, 
+        dropout=0.3, 
+        hidden_dim=256,
+        mode="multimodal"
+    ):
         super(MediLLMModel, self).__init__() # Use constructor of nn.Module
+        assert mode in ["text", "image", "multimodal"], "Mode must be one of: 'text', 'image', or 'multimodal'"
+        self.mode = mode.lower()
 
         # Text encoder: Bio_ClinicalBERT
         self.text_encoder = AutoModel.from_pretrained(text_model_name) # Automodel returns base model without a classification head, just embeddings
@@ -25,11 +34,19 @@ class MediLLMModel(nn.Module):
           ↓
         ReLU
         """
+        # Image encoder
         self.image_encoder = timm.create_model("resnet50", pretrained=True, num_classes=0) # Model for images ResNet-50 which means 50 layers, Intial Conv + pooling layer(size reduction), 16 residual blocks * 3 layers per block = 48 layers, Final Fully-connected layer, each block has 3 conv layers + skip connection i.e input is fed into the nex block with the ouput (F(x) + x)
         self.image_hidden_size = self.image_encoder.num_features # Size of ResNet output ---> 2048 for ResNet-50
 
-        # Fusion layer: concatenate + FC
-        fusion_dim = self.text_hidden_size + self.image_hidden_size # Total size after concatenating text and image features
+        # --- Determine fusion dimension based on the mode ---
+        if self.mode == "text":
+          fusion_dim = self.text_hidden_size
+        elif self.mode == "image":
+          fusion_dim = self.image_hidden_size
+        else:
+          fusion_dim = self.text_hidden_size + self.image_hidden_size        
+        
+        # --- Classification head ---
         self.classifier = nn.Sequential(   # pass the combined features into the classifier
             nn.Linear(fusion_dim, hidden_dim), # Dense layer
             nn.ReLU(), # Non-linear activation function
@@ -37,20 +54,22 @@ class MediLLMModel(nn.Module):
             nn.Linear(hidden_dim, num_classes) # Final Classification output
         )
 
-    def forward(self, input_ids, attention_mask, image_tensor): # input_ids shape: [batch, seq_length], attention_mask: mask to ignore padding same shape, image_tensor: [batch, 3, 224, 224]
-        # Text features
+    def forward(self, input_ids=None, attention_mask=None, image=None): # input_ids shape: [batch, seq_length], attention_mask: mask to ignore padding same shape, image_tensor: [batch, 3, 224, 224]
+      # Text features
+      if self.mode == "text":
         text_outputs = self.text_encoder(input_ids=input_ids, attention_mask=attention_mask) # feed tokenized text into the BERT Model which returns a dictionary with last_hidden_state: [batch_size, seq_len, hidden_size], pooler_output: [batch_szie, hidden_size](CLS embeddings), hidden_states: List of tensors, attentions(weights): List of Tensors
-        text_feat = text_outputs.last_hidden_state[:, 0, :] # CLS token, return CLS tokens from all batches, position 0, a batch of 3 sentences has 3 CLS tokens 
+        features = text_outputs.last_hidden_state[:, 0, :] # CLS token, return CLS tokens from all batches, position 0, a batch of 3 sentences has 3 CLS tokens 
 
-        # Image features
-        image_feat = self.image_encoder(image_tensor) # pass the image through ResNet, returns a [batch, 2048] tensor 
+      # Image features
+      elif self.mode == "image":
+        features = self.image_encoder(image) # pass the image through ResNet, returns a [batch, 2048] tensor 
 
-        # Concatenate features
-        combined = torch.cat((text_feat, image_feat), dim=1) # Concatenates text and image features along feature dimension [CLS vector from BERT] + [ResNet image vector] -> [batch_size, 2816]
+      else: # multimodal
+        text_outputs = self.text_encoder(input_ids=input_ids, attention_mask=attention_mask)
+        text_feat = text_outputs.last_hidden_state[:, 0, :] #CLS token
+        image_feat = self.image_encoder(image)
+        features = torch.cat((text_feat, image_feat), dim=1) # Concatenates text and image features along feature dimension [CLS vector from BERT] + [ResNet image vector] -> [batch_size, 2816]
 
-        # Classification head
-        output = self.classifier(combined) # feeds the combined feature into the classifier, final output shape: [batch, num_classes]
-
-        return output # Return logits for each class, later apply softmax during evaluation
+      return self.classifier(features) # Return logits for each class, later apply softmax during evaluation
     
     
