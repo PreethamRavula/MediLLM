af86b3665ec1a2234437c88d3b5988a21870a506
Sri Preetham Sai Ravula <preethamsai.ravula@gmail.com>
2025-07-03 20:26:33 -0400

    made formatting changes for CI pipeline

--- src/multimodal_model.py
+++ src/multimodal_model.py
@@ -1,25 +1,40 @@
 import torch  # Deep learning framework
-import torch.nn as nn 
-import timm # For Image models like ResNet
-from transformers import AutoModel # Pretrained text encoders
+import torch.nn as nn
+import timm  # For Image models like ResNet
+from transformers import AutoModel  # Pretrained text encoders
 
 
 class MediLLMModel(nn.Module):
     def __init__(
-        self, 
-        text_model_name="emilyalsentzer/Bio_ClinicalBERT", # Bio_ClinicalBERT is a pretrained model on clinical notes, output to 3 classes i.e triage levels
-        num_classes=3, 
-        dropout=0.3, 
+        self,
+        text_model_name=(
+            "emilyalsentzer/Bio_ClinicalBERT"
+        ),
+        # Bio_ClinicalBERT is a pretrained model on clinical notes,
+        # output to 3 classes i.e triage levels
+        num_classes=3,
+        dropout=0.3,
         hidden_dim=256,
-        mode="multimodal"
+        mode="multimodal",
     ):
-        super(MediLLMModel, self).__init__() # Use constructor of nn.Module
-        assert mode in ["text", "image", "multimodal"], "Mode must be one of: 'text', 'image', or 'multimodal'"
+        super(MediLLMModel, self).__init__()  # Use constructor of nn.Module
+        assert mode in [
+            "text",
+            "image",
+            "multimodal",
+        ], "Mode must be one of: 'text', 'image', or 'multimodal'"
         self.mode = mode.lower()
 
         # Text encoder: Bio_ClinicalBERT
-        self.text_encoder = AutoModel.from_pretrained(text_model_name) # Automodel returns base model without a classification head, just embeddings
-        self.text_hidden_size = self.text_encoder.config.hidden_size # Dimensionality of hidden states i.e embedding vector size returned by the text_encoder for each token, 768 for Bert models
+        self.text_encoder = AutoModel.from_pretrained(
+            text_model_name
+        )  # Automodel returns base model without a classification head,
+        # just embeddings
+        self.text_hidden_size = (
+            self.text_encoder.config.hidden_size
+        )
+        # Dimensionality of hidden states i.e embedding vector size returned by
+        # the text_encoder for each token, 768 for Bert models
 
         # Image encoder: ResNet-50 via TIMM
         """
@@ -35,41 +50,78 @@ class MediLLMModel(nn.Module):
         ReLU
         """
         # Image encoder
-        self.image_encoder = timm.create_model("resnet50", pretrained=True, num_classes=0) # Model for images ResNet-50 which means 50 layers, Intial Conv + pooling layer(size reduction), 16 residual blocks * 3 layers per block = 48 layers, Final Fully-connected layer, each block has 3 conv layers + skip connection i.e input is fed into the nex block with the ouput (F(x) + x)
-        self.image_hidden_size = self.image_encoder.num_features # Size of ResNet output ---> 2048 for ResNet-50
+        self.image_encoder = timm.create_model(
+            "resnet50", pretrained=True, num_classes=0
+        )  # Model for images ResNet-50 which means 50 layers.
+        # Initial Conv + pooling layer (size reduction),
+        # 16 residual blocks * 3 layers per block = 48 layers,
+        # Final Fully-connected layer.
+        # Each block has 3 conv layers + skip connection,
+        # i.e. input is fed into the next block with the output (F(x) + x)
+        self.image_hidden_size = (
+            self.image_encoder.num_features
+        )  # Size of ResNet output ---> 2048 for ResNet-50
 
         # --- Determine fusion dimension based on the mode ---
         if self.mode == "text":
-          fusion_dim = self.text_hidden_size
+            fusion_dim = self.text_hidden_size
         elif self.mode == "image":
-          fusion_dim = self.image_hidden_size
+            fusion_dim = self.image_hidden_size
         else:
-          fusion_dim = self.text_hidden_size + self.image_hidden_size        
-        
+            fusion_dim = self.text_hidden_size + self.image_hidden_size
+
         # --- Classification head ---
-        self.classifier = nn.Sequential(   # pass the combined features into the classifier
-            nn.Linear(fusion_dim, hidden_dim), # Dense layer
-            nn.ReLU(), # Non-linear activation function
-            nn.Dropout(dropout), # randomly Zeroes 30 percent of neuron outputs to prevent over-fitting
-            nn.Linear(hidden_dim, num_classes) # Final Classification output
+        # pass the combined features into the classifier
+        self.classifier = nn.Sequential(
+            nn.Linear(fusion_dim, hidden_dim),  # Dense layer
+            nn.ReLU(),  # Non-linear activation function
+            nn.Dropout(
+                dropout
+            ),  # randomly zeroes 30 percent of neuron outputs
+            # to prevent over-fitting
+            nn.Linear(hidden_dim, num_classes),  # Final Classification output
         )
 
-    def forward(self, input_ids=None, attention_mask=None, image=None): # input_ids shape: [batch, seq_length], attention_mask: mask to ignore padding same shape, image_tensor: [batch, 3, 224, 224]
-      # Text features
-      if self.mode == "text":
-        text_outputs = self.text_encoder(input_ids=input_ids, attention_mask=attention_mask) # feed tokenized text into the BERT Model which returns a dictionary with last_hidden_state: [batch_size, seq_len, hidden_size], pooler_output: [batch_szie, hidden_size](CLS embeddings), hidden_states: List of tensors, attentions(weights): List of Tensors
-        features = text_outputs.last_hidden_state[:, 0, :] # CLS token, return CLS tokens from all batches, position 0, a batch of 3 sentences has 3 CLS tokens 
+    def forward(
+        self, input_ids=None, attention_mask=None, image=None
+    ):
+        # input_ids shape: [batch, seq_length]
+        # attention_mask: mask to ignore padding, same shape as input_ids
+        # image: [batch, 3, 224, 224]
+        # Text features
+        if self.mode == "text":
+            text_outputs = self.text_encoder(
+                input_ids=input_ids, attention_mask=attention_mask
+            )
+            # feed tokenized text into the BERT Model which returns a
+            # dictionary with last_hidden_state: [batch_size, seq_len,
+            # hidden_size], pooler_output: [batch_size, hidden_size]
+            # (CLS embeddings), hidden_states: List of tensors,
+            # attentions(weights): List of Tensors
+            features = text_outputs.last_hidden_state[
+                :, 0, :
+            ]  # CLS token, return CLS tokens from all batches, position 0,
+            # a batch of 3 sentences has 3 CLS tokens
 
-      # Image features
-      elif self.mode == "image":
-        features = self.image_encoder(image) # pass the image through ResNet, returns a [batch, 2048] tensor 
+        # Image features
+        elif self.mode == "image":
+            features = self.image_encoder(
+                image
+            )  # pass the image through ResNet, returns a [batch, 2048] tensor
 
-      else: # multimodal
-        text_outputs = self.text_encoder(input_ids=input_ids, attention_mask=attention_mask)
-        text_feat = text_outputs.last_hidden_state[:, 0, :] #CLS token
-        image_feat = self.image_encoder(image)
-        features = torch.cat((text_feat, image_feat), dim=1) # Concatenates text and image features along feature dimension [CLS vector from BERT] + [ResNet image vector] -> [batch_size, 2816]
+        else:  # multimodal
+            text_outputs = self.text_encoder(
+                input_ids=input_ids, attention_mask=attention_mask
+            )
+            text_feat = text_outputs.last_hidden_state[:, 0, :]  # CLS token
+            image_feat = self.image_encoder(image)
+            features = torch.cat(
+                (text_feat, image_feat), dim=1
+            )  # Concatenates text and image features along feature dimension
+            # [CLS vector from BERT] + [ResNet image vector]
+            # -> [batch_size, 2816]
 
-      return self.classifier(features) # Return logits for each class, later apply softmax during evaluation
-    
-    
+        # Return logits for each class, later apply softmax during evaluation
+        return self.classifier(
+            features
+        )
--- src/train.py
+++ src/train.py
@@ -1,17 +1,29 @@
-import torch # PyTorch core utility for model training 
+import torch  # PyTorch core utility for model training
 import os
 import sys
 import yaml
 import argparse
-import matplotlib.pyplot as plt # for plotting
+import matplotlib.pyplot as plt  # for plotting
 
-from tqdm import tqdm # loading bar for loops
-from torch.utils.data import DataLoader, Subset # Dataloader to batch and feed data to model, random split to split dataset into train and validation sets
-from torch.nn import CrossEntropyLoss # PyTorch core utility for model training
-from torch.optim import Adam # PyTorch core utility for model training, Adam is the Optimizer a gradient descent model
-from sklearn.metrics import accuracy_score, f1_score # Evaluation metrics
+from tqdm import tqdm  # loading bar for loops
+from torch.utils.data import (
+    DataLoader,
+    Subset,
+)  # Dataloader to batch and feed data to model,
+
+# random split to split dataset into train and validation sets
+from torch.nn import (
+    CrossEntropyLoss,
+)  # PyTorch core utility for model training
+from torch.optim import Adam  # PyTorch core utility for model training,
+
+# Adam is the Optimizer, a gradient descent model
+from sklearn.metrics import accuracy_score, f1_score  # Evaluation metrics
 from sklearn.model_selection import StratifiedShuffleSplit
 
+
+from src.triage_dataset import TriageDataset  # Dataset Class
+from src.multimodal_model import MediLLMModel  # Mutlimodal Model
 
 # Setup base path
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
@@ -20,9 +32,6 @@ base_dir = os.path.dirname(os.path.dirname(__file__))
 if base_dir not in sys.path:
     sys.path.append(base_dir)
 
-
-from src.triage_dataset import TriageDataset # Dataset Class
-from src.multimodal_model import MediLLMModel # Mutlimodal Model
 
 def load_config(mode):
     config_path = os.path.join(base_dir, "config", "config.yaml")
@@ -36,73 +45,86 @@ def load_config(mode):
                 "dropout": 0.3,
                 "hidden_dim": 256,
                 "batch_size": 8,
-                "epochs": 5
+                "epochs": 5,
             },
             "image": {
                 "lr": 2e-5,
                 "dropout": 0.3,
                 "hidden_dim": 256,
                 "batch_size": 8,
-                "epochs": 5
+                "epochs": 5,
             },
             "multimodal": {
                 "lr": 2e-5,
                 "dropout": 0.3,
                 "hidden_dim": 256,
                 "batch_size": 8,
-                "epochs": 5
-            }
+                "epochs": 5,
+            },
         }
         with open(config_path, "w") as f:
             yaml.dump(default_config, f)
-    
     # otherwise export to yaml
     with open(config_path, "r") as f:
         config = yaml.safe_load(f)
 
     if mode not in config:
         raise ValueError(f"No config found for mode '{mode}' in config.yaml")
-    
     return config[mode]
+
 
 def stratified_split(dataset, val_ratio=0.2, seed=42):
     labels = [dataset.df.iloc[i]["triage_level"] for i in range(len(dataset))]
-    sss = StratifiedShuffleSplit(n_splits=1, test_size=val_ratio, random_state=seed)
+    sss = StratifiedShuffleSplit(
+        n_splits=1,
+        test_size=val_ratio,
+        random_state=seed,
+    )
     tran_idx, val_idx = next(sss.split(range(len(dataset)), labels))
     return Subset(dataset, tran_idx), Subset(dataset, val_idx)
 
-def train_model(mode="multimodal"): # Function to instantiate model and data, train, validate, plot results and save the model
+
+# Function to instantiate model and data, train, validate, plot results
+# and save the model
+def train_model(mode="multimodal"):
     cfg = load_config(mode)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # Use GPU if available or else use CPU
-    
+    device = torch.device(
+        "cuda" if torch.cuda.is_available() else "cpu"
+    )  # Use GPU if available or else use CPU
+
     dataset_dir = os.path.join(base_dir, "data", "emr_records.csv")
-    dataset = TriageDataset(
-        csv_file=dataset_dir,
-        mode=mode
-    )
+    dataset = TriageDataset(csv_file=dataset_dir, mode=mode)
 
     model = MediLLMModel(
-        dropout=cfg["dropout"],
-        hidden_dim=cfg["hidden_dim"],
-        mode = mode
-    ).to(device) # moves the model to selected device
- 
+        dropout=cfg["dropout"], hidden_dim=cfg["hidden_dim"], mode=mode
+    ).to(
+        device
+    )  # moves the model to selected device
+
     train_set, val_set = stratified_split(dataset)
     batch_size = cfg["batch_size"]
 
-    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True) # Create data in batches to the model
+    train_loader = DataLoader(
+        train_set, batch_size=batch_size, shuffle=True
+    )  # Create data in batches to the model
     val_loader = DataLoader(val_set, batch_size=batch_size)
 
-    criterion = CrossEntropyLoss() # Calculate difference between model prediction and true labels
-    optimizer = Adam(model.parameters(), lr=cfg["lr"]) # Adaptive learning rate optimizer for fast-converging
+    # Calculate difference between model prediction and true labels
+    criterion = CrossEntropyLoss()
+    optimizer = Adam(
+        model.parameters(), lr=cfg["lr"]
+    )  # Adaptive learning rate optimizer for fast-converging
 
-    train_acc, val_acc = [], [] # Lists to store accuracy per epoch for plotting
+    # Lists to store accuracy per epoch for plotting
+    train_acc, val_acc = [], []
 
     for epoch in range(cfg["epochs"]):
-        model.train() # Activate training the model, enable dropout
+        model.train()  # Activate training the model, enable dropout
         all_preds, all_labels = [], []
 
-        for batch in tqdm(train_loader, desc=f"[{mode}] Epoch {epoch+1}"): # Load a batch of text, images, and labels to GPU or CPU
+        for batch in tqdm(
+            train_loader, desc=f"[{mode}] Epoch {epoch+1}"
+        ):  # Load a batch of text, images, and labels to GPU or CPU
             input_ids = batch.get("input_ids", None)
             attention_mask = batch.get("attention_mask", None)
             images = batch.get("image", None)
@@ -123,29 +145,56 @@ def train_model(mode="multimodal"): # Function to instantiate model and data, tr
                     "label":           torch.Size([8])
                 }
             """
-            optimizer.zero_grad() # Zero out gradients from previous batch
-            outputs = model(input_ids=input_ids, attention_mask=attention_mask, image=images) # Forward pass through the model
-            loss = criterion(outputs, labels) # Compute loss value
-            loss.backward() # Back propagation to compute gradients
-            optimizer.step() # Adjust the weights using gradients
+            optimizer.zero_grad()  # Zero out gradients from previous batch
+            outputs = model(
+                input_ids=input_ids,
+                attention_mask=attention_mask,
+                image=images,
+            )  # Forward pass through the model
+            loss = criterion(outputs, labels)  # Compute loss value
+            loss.backward()  # Back propagation to compute gradients
+            optimizer.step()  # Adjust the weights using gradients
 
-            preds = torch.argmax(outputs, dim=1).cpu().numpy() # Get the predicted class per sample and covert to CPU & Numpy for easier comparision
-            all_preds.extend(preds) # Save predictions for metric computation, extend() appends each element of preds to the list
-            all_labels.extend(labels.cpu().numpy()) # Save labels for metric computation
-        
+            preds = torch.argmax(outputs, dim=1).cpu().numpy()
+            # Get the predicted class per sample and convert to CPU & Numpy for
+            # easier comparison
+            all_preds.extend(preds)
+            # Save predictions for metric computation.
+            # extend() appends each element of preds to the list
+            all_labels.extend(
+                labels.cpu().numpy()
+            )  # Save labels for metric computation
+
         # Calculating classification metrics (Accuracy and F1)
-        acc = accuracy_score(all_labels, all_preds) # Evaluate full-epoch performance
-        f1 = f1_score(all_labels, all_preds, average="weighted") # 1) binary: Binary Classification(F1 score of +ve class only); 2) macro: Computes F1 for each class independently, then averages, treats all classes equally; 3) micro: Flattens all true and predicted labels and the computes global TP, FP, FN and gets F1 from that, works well with imbalanced data, equal to accuracy in binary classification and different in multi-class/multi-label; 4) Weighted: calculates F1 for each class, then averages them using number of samples, avoids bias, real-world and imbalanced classes, per-class performance; 5) samples: used for multi-label classification, computes F1 for each instance, then averages across all samples, row-wise, not class-wise
-        train_acc.append(acc) # Append to a list for plotting
+        acc = accuracy_score(
+            all_labels, all_preds
+        )  # Evaluate full-epoch performance
+        f1 = f1_score(all_labels, all_preds, average="weighted")
+        # 1) binary: Binary Classification(F1 score of +ve class only)
+        # 2) macro: Computes F1 for each class independently, then averages,
+        #    treats all classes equally
+        # 3) micro: Flattens all true and predicted labels and then computes
+        #    global TP, FP, FN and gets F1 from that, works well with
+        #    imbalanced data, equal to accuracy in binary classification and
+        #    different in multi-class/multi-label
+        # 4) weighted: calculates F1 for each class, then averages them using
+        #    number of samples, avoids bias, real-world and imbalanced classes,
+        #    per-class performance
+        # 5) samples: used for multi-label classification, computes F1 for each
+        #    instance, then averages across all samples, row-wise,
+        #    not class-wise
+        train_acc.append(acc)  # Append to a list for plotting
 
         print(f"Train Accuracy: {acc:.4f}, F1 Score: {f1:.4f}")
 
         # Validation loop
-        model.eval() # Deactivates dropnot and batchnorm for inference
+        model.eval()  # Deactivates dropnot and batchnorm for inference
         val_preds, val_labels = [], []
 
-        with torch.no_grad(): # Disables autograd to save memory
-            for batch in val_loader: # Load batch of validation data text, images, labels to GPU or CPU
+        with torch.no_grad():  # Disables autograd to save memory
+            for batch in val_loader:
+                # Load batch of validation data text, images, labels
+                # to GPU or CPU
                 input_ids = batch.get("input_ids", None)
                 attention_mask = batch.get("attention_mask", None)
                 images = batch.get("image", None)
@@ -157,25 +206,43 @@ def train_model(mode="multimodal"): # Function to instantiate model and data, tr
                     attention_mask = attention_mask.to(device)
                 if images is not None:
                     images = images.to(device)
-                
-                outputs = model(input_ids=input_ids, attention_mask=attention_mask, image=images)
+
+                outputs = model(
+                    input_ids=input_ids,
+                    attention_mask=attention_mask,
+                    image=images,
+                )
                 preds = torch.argmax(outputs, dim=1).cpu().numpy()
-                
+
                 val_preds.extend(preds)
                 val_labels.extend(labels.cpu().numpy())
 
-        val_acc_epoch = accuracy_score(val_labels, val_preds) # Validation metrics, accuracy: how mant items did your model get right out of total items
-        val_f1 = f1_score(val_labels, val_preds, average="weighted") # F1 score weights in both precision and recall, uses hoarmonic mean to punish imbalance, if one of the 2 is low it drags the accuracy score down, precision: How careful is the model when classfying an item (TP / (TP + FP)), Recall: Hom many real items did it actually spot (TP / (TP + FN))
+        val_acc_epoch = accuracy_score(
+            val_labels, val_preds
+        )  # Validation metrics, accuracy: how many items did your model get
+        # right out of total items
+        val_f1 = f1_score(val_labels, val_preds, average="weighted")
+        # F1 score weights in both precision and recall, uses harmonic mean to
+        # punish imbalance. If one of the two is low, it drags the accuracy
+        # score down.
+        # Precision: How careful is the model when classifying an item
+        # (TP / (TP + FP)).
+        # Recall: How many real items did it actually spot
+        # (TP / (TP + FN)).
         val_acc.append(val_acc_epoch)
 
         print(f"Val Accuracy: {val_acc_epoch:.4f}, F1 Score: {val_f1:.4f}")
 
     # Save model
     model_path = os.path.join(base_dir, f"medi_llm_model_{mode}.pth")
-    torch.save(model.state_dict(), model_path) # Saves the model weights only not total architecture to reuse later
+    torch.save(
+        model.state_dict(), model_path
+    )  # Saves the model weights only not total architecture to reuse later
 
     # Plot accuracy
-    plot_path = os.path.join(base_dir, "assets", f"model_training_curve_{mode}.png")
+    plot_path = os.path.join(
+        base_dir, "assets", f"model_training_curve_{mode}.png"
+    )
     plt.plot(train_acc, label="Train Acc")
     plt.plot(val_acc, label="Val Acc")
     plt.legend()
@@ -183,14 +250,17 @@ def train_model(mode="multimodal"): # Function to instantiate model and data, tr
     plt.savefig(plot_path)
     print(f"✅ Saved training curve to {plot_path}")
 
+
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--mode", choices=["text", "image", "multimodal"], default="multimodal")
+    parser.add_argument(
+        "--mode", choices=["text", "image", "multimodal"], default="multimodal"
+    )
     return parser.parse_args()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     args = parse_args()
-    train_model(mode=args.mode) # Only runs if file is run directly not when it is imported
-
-
-
+    train_model(
+        mode=args.mode
+    )  # Only runs if file is run directly not when it is imported
